11ccc74b8f464395e7afa59107034fb7847b8fe9
Hansadhwaja <hansadhwajabiswal@gmail.com>
2025-08-29 17:55:30 +0530

    Fixed products sorting by name inside each category

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -4,11 +4,10 @@ import connectToDatabase from "@/lib/db";
 import { Product } from "@/lib/models/Product";
 import { ProductData } from "@/types";
 
-
-
 export async function GET() {
     await connectToDatabase();
     const products = await Product.aggregate([
+        { $sort: { category: 1, name: 1 } },
         {
             $group: {
                 _id: "$category",
